abae9d8d19d1f33b2c61337a2913a1453e418fe7
liujie <liujie045.china@outlook.com>
2018-05-28 15:24:09 +0800

    Handy Commit

--- library/src/main/java/com/handy/qrcode/camera/AutoFocusManager.java
+++ library/src/main/java/com/handy/qrcode/camera/AutoFocusManager.java
@@ -23,7 +23,7 @@ import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-import com.handy.qrcode.PreferencesActivity;
+import com.handy.qrcode.Preferences;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -53,9 +53,7 @@ final class AutoFocusManager implements Camera.AutoFocusCallback {
         this.camera = camera;
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         String currentFocusMode = camera.getParameters().getFocusMode();
-        useAutoFocus =
-                sharedPrefs.getBoolean(PreferencesActivity.KEY_AUTO_FOCUS, true) &&
-                        FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
+        useAutoFocus = Preferences.KEY_AUTO_FOCUS && FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
         Log.i(TAG, "Current focus mode '" + currentFocusMode + "'; use auto focus? " + useAutoFocus);
         start();
     }
@@ -130,5 +128,4 @@ final class AutoFocusManager implements Camera.AutoFocusCallback {
             return null;
         }
     }
-
 }
