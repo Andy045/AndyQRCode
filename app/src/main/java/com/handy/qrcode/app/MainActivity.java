18f415cf1b4d5f9c55615c05f73bcf46ea921442
liujie <liujie045.china@outlook.com>
2018-05-29 17:57:57 +0800

    Handy Commit

--- app/src/main/java/com/handy/qrcode/app/MainActivity.java
+++ app/src/main/java/com/handy/qrcode/app/MainActivity.java
@@ -1,9 +1,11 @@
 package com.handy.qrcode.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.handy.base.app.BaseActivity;
-import com.handy.base.utils.IntentUtils;
 import com.handy.qrcode.CaptureActivity;
 
 public class MainActivity extends BaseActivity {
@@ -13,6 +15,21 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.zxing).setOnClickListener(v -> IntentUtils.openActivity(activity, CaptureActivity.class, false));
+        findViewById(R.id.zxing).setOnClickListener(v -> {
+            CaptureActivity.doIntent(activity, new Bundle(), (rawResult, barcode, scaleFactor) -> {
+                Log.d("==============", rawResult.getText());
+                Toast.makeText(activity, rawResult.getText(), Toast.LENGTH_SHORT).show();
+            }, 1, false);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null && data.getExtras().size() > 0) {
+                Log.d("++++++++++++++++++", data.getStringExtra("Result"));
+            }
+        }
     }
 }
