9a4f19701f1b77edcc406d529c4264abe9d1527e
liujie <liujie045.china@outlook.com>
2018-05-30 16:13:19 +0800

    Handy Commit

--- app/src/main/java/com/handy/qrcode/app/MainActivity.java
+++ app/src/main/java/com/handy/qrcode/app/MainActivity.java
@@ -7,20 +7,10 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.handy.qrcode.ScanActivity;
+import com.handy.qrcode.ScanSingleBuild;
 import com.handy.qrcode.utils.BitmapUtils;
 
 public class MainActivity extends Activity {
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            if (data != null && data.getExtras() != null && data.getExtras().size() > 0) {
-                Log.d("++++++++++++++++++", data.getStringExtra("Result"));
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +21,22 @@ public class MainActivity extends Activity {
             ((TextView) findViewById(R.id.result)).setText("");
             ((ImageView) findViewById(R.id.image)).setImageBitmap(null);
 
-            ScanActivity.doIntent(MainActivity.this, new Bundle(), (rawResult, barcode, scaleFactor) -> {
+            new ScanSingleBuild().setScanResultListener((rawResult, barcode, scaleFactor) -> {
                 BitmapUtils.drawResultPoints(MainActivity.this, barcode, scaleFactor, rawResult);
 
                 ((TextView) findViewById(R.id.result)).setText("扫描结果：" + rawResult.getText());
                 ((ImageView) findViewById(R.id.image)).setImageBitmap(barcode);
-            }, 1, false);
+            }).start(MainActivity.this);
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null && data.getExtras().size() > 0) {
+                Log.d("++++++++++++++++++", data.getStringExtra("Result"));
+            }
+        }
     }
 }
