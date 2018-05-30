586a96bb3847f569bbb286b931fc94695b695c50
liujie <liujie045.china@outlook.com>
2018-05-30 11:07:24 +0800

    Handy Commit

---
+++ library/src/main/java/com/handy/qrcode/utils/LogUtils.java
@@ -0,0 +1,51 @@
+package com.handy.qrcode.utils;
+
+import android.util.Log;
+
+/**
+ * 类名
+ *
+ * @author LiuJie https://www.Handy045.com
+ * @description 类功能内容
+ * @date Created in 2018/5/30 上午10:45
+ * @modified By LiuJie
+ */
+public class LogUtils {
+    public static int LOG_LEVEL = 0;
+    public static int ERROR = 1;
+    public static int WARN = 2;
+    public static int INFO = 3;
+    public static int DEBUG = 4;
+    public static int VERBOS = 5;
+
+
+    public static void e(String tag, String msg) {
+        if (LOG_LEVEL > ERROR) {
+            Log.e(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg) {
+        if (LOG_LEVEL > WARN) {
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (LOG_LEVEL > INFO) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (LOG_LEVEL > DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void v(String tag, String msg) {
+        if (LOG_LEVEL > VERBOS) {
+            Log.v(tag, msg);
+        }
+    }
+}
