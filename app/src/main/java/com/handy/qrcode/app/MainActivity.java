3df4aeafae5f63fb66356987673cc0d8d1b15713
liujie <liujie045.china@outlook.com>
2018-05-30 09:37:06 +0800

    Handy Commit

--- app/src/main/java/com/handy/qrcode/app/MainActivity.java
+++ app/src/main/java/com/handy/qrcode/app/MainActivity.java
@@ -1,26 +1,27 @@
 package com.handy.qrcode.app;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import com.google.zxing.ResultPoint;
 import com.handy.base.app.BaseActivity;
 import com.handy.qrcode.CaptureActivity;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.zxing).setOnClickListener(v -> {
-            CaptureActivity.doIntent(activity, new Bundle(), (rawResult, barcode, scaleFactor) -> {
-                Log.d("==============", rawResult.getText());
-                Toast.makeText(activity, rawResult.getText(), Toast.LENGTH_SHORT).show();
-            }, 1, false);
-        });
+    private static void drawLine(Canvas canvas, Paint paint, ResultPoint a, ResultPoint b, float scaleFactor) {
+        if (a != null && b != null) {
+            canvas.drawLine(scaleFactor * a.getX(), scaleFactor * a.getY(), scaleFactor * b.getX(), scaleFactor * b.getY(), paint);
+        }
     }
 
     @Override
@@ -29,6 +30,50 @@ public class MainActivity extends BaseActivity {
         if (requestCode == 1 && resultCode == RESULT_OK) {
             if (data != null && data.getExtras() != null && data.getExtras().size() > 0) {
                 Log.d("++++++++++++++++++", data.getStringExtra("Result"));
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.zxing).setOnClickListener(v -> {
+            ((TextView) findViewById(R.id.result)).setText("");
+            ((ImageView) findViewById(R.id.image)).setImageBitmap(null);
+
+            CaptureActivity.doIntent(activity, new Bundle(), (rawResult, barcode, scaleFactor) -> {
+                Log.d("==============", rawResult.getText());
+                Toast.makeText(activity, rawResult.getText(), Toast.LENGTH_SHORT).show();
+                drawResultPoints(barcode, scaleFactor, rawResult);
+
+                ((TextView) findViewById(R.id.result)).setText("扫描结果：" + rawResult.getText());
+                ((ImageView) findViewById(R.id.image)).setImageBitmap(barcode);
+            }, 1, false);
+        });
+    }
+
+    private void drawResultPoints(Bitmap barcode, float scaleFactor, Result rawResult) {
+        ResultPoint[] points = rawResult.getResultPoints();
+        if (points != null && points.length > 0) {
+            Canvas canvas = new Canvas(barcode);
+            Paint paint = new Paint();
+            paint.setColor(getResources().getColor(R.color.result_points));
+            if (points.length == 2) {
+                paint.setStrokeWidth(4.0f);
+                drawLine(canvas, paint, points[0], points[1], scaleFactor);
+            } else if (points.length == 4 && (rawResult.getBarcodeFormat() == BarcodeFormat.UPC_A || rawResult.getBarcodeFormat() == BarcodeFormat.EAN_13)) {
+                // Hacky special case -- draw two lines, for the barcode and metadata
+                drawLine(canvas, paint, points[0], points[1], scaleFactor);
+                drawLine(canvas, paint, points[2], points[3], scaleFactor);
+            } else {
+                paint.setStrokeWidth(10.0f);
+                for (ResultPoint point : points) {
+                    if (point != null) {
+                        canvas.drawPoint(scaleFactor * point.getX(), scaleFactor * point.getY(), paint);
+                    }
+                }
             }
         }
     }
