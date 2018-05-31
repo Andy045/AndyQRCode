6d13ecfe60ae235f8cd6a9ab821a761f5bc1c161
liujie <liujie045.china@outlook.com>
2018-05-31 17:07:36 +0800

    Handy Commit

--- app/src/main/java/com/handy/qrcode/app/MainActivity.java
+++ app/src/main/java/com/handy/qrcode/app/MainActivity.java
@@ -1,11 +1,9 @@
 package com.handy.qrcode.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,24 +21,18 @@ public class MainActivity extends Activity {
             ((TextView) findViewById(R.id.result)).setText("");
             ((ImageView) findViewById(R.id.image)).setImageBitmap(null);
 
-            new ScanSingleBuild().setScanResultListener((rawResult, barcode, scaleFactor) -> {
-                Bitmap bitmap;
-                bitmap = BitmapUtils.drawResultPoints(MainActivity.this, barcode, scaleFactor, rawResult, true);
-                bitmap = BitmapUtils.compressByScale(bitmap, 360, 640, true);
-                bitmap = BitmapUtils.addTextWatermark(MainActivity.this, bitmap, "HandyQRCode\nhttps://www.handy045.com", 11, Color.BLUE, 4, 4, true);
-                ((TextView) findViewById(R.id.result)).setText("扫描结果：" + rawResult.getText());
-                ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
-            }).start(MainActivity.this);
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            if (data != null && data.getExtras() != null && data.getExtras().size() > 0) {
-                Log.d("++++++++++++++++++", data.getStringExtra("Result"));
+            try {
+                new ScanSingleBuild().setScanResultListener((rawResult, barcode, scaleFactor) -> {
+                    Bitmap bitmap;
+                    bitmap = BitmapUtils.drawResultPoints(MainActivity.this, barcode, scaleFactor, rawResult, true);
+                    bitmap = BitmapUtils.compressByScale(bitmap, 360, 640, true);
+                    bitmap = BitmapUtils.addTextWatermark(MainActivity.this, bitmap, "HandyQRCode\nhttps://www.handy045.com", 42, Color.BLUE, 4, 4, true);
+                    ((TextView) findViewById(R.id.result)).setText("扫描结果：" + rawResult.getText());
+                    ((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
+                }).start(MainActivity.this);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }
+        });
     }
 }
