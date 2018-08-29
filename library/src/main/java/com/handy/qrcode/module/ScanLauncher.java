f73aa115583d1bb5cf34c2f3fdda1a95a2724697
liujie <liujie045.china@outlook.com>
2018-08-29 14:43:12 +0800

    Handy Commit

--- library/src/main/java/com/handy/qrcode/module/ScanLauncher.java
+++ library/src/main/java/com/handy/qrcode/module/ScanLauncher.java
@@ -1,11 +1,17 @@
 package com.handy.qrcode.module;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.handy.qrcode.utils.LogUtils;
-import com.handy.qrcode.utils.PermissionsUtils;
+
+import java.util.ArrayList;
 
 /**
  * 扫描功能创建者
@@ -28,10 +34,36 @@ public class ScanLauncher {
      */
     public void startSingle(@NonNull Activity activity, @NonNull ScanConfig.ScanResultListener scanResultListener) {
         LogUtils.init(activity.getApplication());
-        if (!PermissionsUtils.checkDeniedPermissions(activity, true)) {
+        if (!checkDeniedPermissions(activity, true)) {
             ScanConfig.scanResultListener = null;
             ScanConfig.scanResultListener = scanResultListener;
             activity.startActivity(new Intent(activity, ScanSingleActivity.class));
         }
+    }
+
+    /**
+     * 扫描权限集合 如果发现未启用权限，停止扫描并启动权限动态请求。
+     *
+     * @param activity  发起请求的Activity
+     * @param isRequest 是否发起系统权限请求
+     * @return 如果有未启用的权限返回true
+     */
+    public static boolean checkDeniedPermissions(Activity activity, boolean isRequest) {
+        ArrayList<String> permissions = new ArrayList<String>() {{
+            add(Manifest.permission.CAMERA);
+            add(Manifest.permission.VIBRATE);
+        }};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (int index = 0; index < permissions.size(); index++) {
+                if (ContextCompat.checkSelfPermission(activity, permissions.get(index)) == PackageManager.PERMISSION_DENIED) {
+                    if (isRequest) {
+                        ActivityCompat.requestPermissions(activity, new String[]{permissions.get(index)}, index);
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
